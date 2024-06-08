30eca96a091d87c5c50d52e2680f6a3d6e9f7b13
Shiv Singh <shivsingh7150@hotmail.com>
2024-06-08 23:37:49 +0530

    Update buy.blade.php

--- resources/views/frontend/pages/buy.blade.php
+++ resources/views/frontend/pages/buy.blade.php
@@ -485,8 +485,8 @@
                             map.setTilt(50);
 
                             // Multiple markers location, latitude, and longitude
-                            var markers = {{ $markers }};
-                            var infoWindowContent = {{ $infowindow }};
+                            var markers = '';
+                            var infoWindowContent = '';
 
                             for (var i = 0; i < markers.length; i++) {
                                 var markerData = markers[i];
