c609b9378244c8b03291d0f7af5ea1102ddd68f8
satyam8076 <168442979+satyam8076@users.noreply.github.com>
2024-08-01 17:46:35 +0530

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
         $this->call(AuthTableSeeder::class);
 
         Schema::enableForeignKeyConstraints();
-
+//gfgfgf
         // User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
