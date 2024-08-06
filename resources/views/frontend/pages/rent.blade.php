2450bd3d7d94e5c901d0548502f6ecc8ace144d6
Ankit Pandita <125086899+AnkitPandita02@users.noreply.github.com>
2024-08-06 18:21:44 +0530

    changes

--- resources/views/frontend/pages/rent.blade.php
+++ resources/views/frontend/pages/rent.blade.php
@@ -5,6 +5,137 @@
 @section('CustomCss')
     <meta name="description" content="test">
     <link rel="stylesheet" href="{{ asset('assets/css/rent.css') }}" />
+    <style>
+        .modal-box {
+  width: 100%;
+  max-width: 500px;
+}
+
+.sd-multiSelect {
+  position: relative;
+}
+.sd-multiSelect .placeholder {
+  opacity: 1;
+  background-color: transparent;
+  cursor: pointer;
+}
+.sd-multiSelect .ms-offscreen {
+  height: 1px;
+  width: 1px;
+  opacity: 0;
+  overflow: hidden;
+  display: none;
+}
+
+.sd-multiSelect .sd-CustomSelect {
+  width: 100% !important;
+}
+
+.sd-multiSelect .ms-choice {
+  position: relative;
+  text-align: left !important;
+  width: 250px;
+  border: 1px solid #e3e3e3;
+  background: #ffff;
+  box-shadow: none;
+  font-size: 15px;
+  height: 44px;
+  font-weight: 500;
+  color: #212529;
+  line-height: 1.5;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  border-radius: 0.25rem;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+}
+
+.sd-multiSelect .ms-choice:after {
+  content: "\f107 ";
+  font-family: "FontAwesome";
+  position: absolute;
+  right: 10px;
+  top: 50%;
+  transform: translateY(-50%);
+  font-size: 18px;
+}
+
+.sd-multiSelect .ms-choice:focus {
+  border-color: var(--theme-color);
+}
+
+.sd-multiSelect .ms-drop.bottom {
+  display: none;
+  background: #fff;
+  border: 1px solid #e5e5e5;
+  padding: 10px;
+}
+
+.sd-multiSelect .ms-drop li {
+  position: relative;
+  margin-bottom: 10px;
+}
+
+.sd-multiSelect .ms-drop li input[type="checkbox"] {
+  padding: 0;
+  height: initial;
+  width: initial;
+  margin-bottom: 0;
+  display: none;
+  cursor: pointer;
+}
+.ms-drop.bottom {
+    width: 250px;
+}
+.sd-multiSelect .ms-drop li label {
+  cursor: pointer;
+  user-select: none;
+  -ms-user-select: none;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+}
+
+/* .sd-multiSelect .ms-drop li label:before {
+  content: "";
+  -webkit-appearance: none;
+  background-color: transparent;
+  border: 2px solid var(--theme-color);
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05),
+    inset 0px -15px 10px -12px rgba(0, 0, 0, 0.05);
+  padding: 8px;
+  display: inline-block;
+  position: relative;
+  vertical-align: middle;
+  cursor: pointer;
+  margin-right: 5px;
+}
+
+.sd-multiSelect .ms-drop li input:checked + span:after {
+  content: "";
+  display: block;
+  position: absolute;
+  top: 9px;
+  left: 5px;
+  width: 10px;
+  height: 10px;
+  background: var(--theme-color);
+  border-width: 0 2px 2px 0;
+} */
+
+    </style>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+  <script src="https://bsite.net/savrajdutta/cdn/multi-select.js"></script>
+  <script>
+    $(document).ready(function() {
+      $(".sd-CustomSelect").multipleSelect({
+        selectAll: false,
+        onOptgroupClick: function(view) {
+          $(view).parents("label").addClass("selected-optgroup");
+        }
+      });
+    });
+  </script>
 @endsection
 @section('content')
     <section class="breadcrumbs" style="background: url(assets/images/rent02.jpg)">
@@ -158,16 +289,20 @@
                                             </div>
                                         </div>
 
-                                        <div class="home_form_label">
-                                            <label>Property Type</label>
-                                            <select class="select_label select3" name="p_type" multiple>
-                                                <option value="Apartment">Apartment</option>
+                                        <div class="modal-box">
+                                            <div class="sd-multiSelect form-group">
+                                              <label for="current-job-role">Property Type</label>
+                                              <select multiple id="current-job-role" class="sd-CustomSelect" aria-placeholder="yggv">
+                                                  {{-- @foreach ($uniquePropertyTypes as $p_type)
+                                                    <option value="{{ $p_type }}">{{ $p_type }}</option>
+                                                @endforeach --}}
+                                                <option value="Apartments">Apartments</option>
                                                 <option value="Villa">Villa</option>
-                                                <option value="Plot">Plot</option>
-                                                <option value="Bungalows">Bungalows</option>
-                                                <option value="Flats">Flats</option>
-                                            </select>
-                                        </div>
+                                                <option value="House">House</option>
+                                                <option value="Hotel">Hotel</option>
+                                              </select>
+                                            </div>
+                                          </div>
 
                                         <div class="home_form_label">
                                             <label>Currency</label>
