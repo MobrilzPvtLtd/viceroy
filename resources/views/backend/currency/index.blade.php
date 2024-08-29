5d699fdb5c65c5c2e4ddcb2756c594169e92b3f4
Shiv Singh <shiv@srapsware.com>
2024-08-29 17:15:25 +0530

    Update currency index page with lazy loading for Bootstrap CSS and modernizr.min.js script

--- resources/views/backend/currency/index.blade.php
+++ resources/views/backend/currency/index.blade.php
@@ -1,137 +1,148 @@
 @extends('backend.layouts.app')
 @section('content')
-<link rel="preload" as="style" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css" />
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
 
-    <div class="card">
-        <div class="card-body">
-            {{-- Display flash messages --}}
-            @if (session('success'))
-                <div class="alert alert-success">
-                    {{ session('success') }}
-                </div>
-            @endif
-
-            {{-- Create Currency Button --}}
-            <div class="pull-right mb-2">
-                <a class="btn btn-success" href="{{ route('currency.create') }}">Create Currency</a>
+<div class="card">
+    <div class="card-body">
+        {{-- Display flash messages --}}
+        @if (session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
             </div>
+        @endif
 
-            <div class="row mt-4">
-                <div class="col">
-                    <div class="table-responsive">
-                        <table id="datatable" class="table table-hover">
-                            <thead>
+        {{-- Create Currency Button --}}
+        <div class="pull-right mb-2">
+            <a class="btn btn-success" href="{{ route('currency.create') }}">Create Currency</a>
+        </div>
+
+        <div class="row mt-4">
+            <div class="col">
+                <div class="table-responsive">
+                    <table id="datatable" class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">Id</th>
+                                <th scope="col">Currency Code</th>
+                                <th scope="col">Prefix</th>
+                                <th scope="col">Suffix</th>
+                                <th scope="col">Base Conv. Rate</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($currencys as $currency)
                                 <tr>
-                                    <th scope="col">Id</th>
-                                    <th scope="col">Currency Code</th>
-                                    <th scope="col">Prefix</th>
-                                    <th scope="col">Suffix</th>
-                                    <th scope="col">Base Conv. Rate</th>
-                                    <th scope="col">Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($currencys as $currency)
-                                    <tr>
-                                        <td>{{ $currency->id }}</td>
-                                        <td>{{ $currency->code }}</td>
-                                        <td>{{ $currency->prefix }}</td>
-                                        <td>{{ $currency->suffix }}</td>
-                                        <td>{{ $currency->bcr }}</td>
-                                        <td>
+                                    <td>{{ $currency->id }}</td>
+                                    <td>{{ $currency->code }}</td>
+                                    <td>{{ $currency->prefix }}</td>
+                                    <td>{{ $currency->suffix }}</td>
+                                    <td>{{ $currency->bcr }}</td>
+                                    <td>
 
                                         <div class="container">
 
-<!-- Button to trigger modal -->
-<a href="#myModal-{{ $currency->id }}" class="btn btn-primary" data-toggle="modal">Launch demo modal</a>
- 
-  <!-- Modal -->
-  <div class="modal fade" id="myModal-{{ $currency->id }}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
-          <h4 class="modal-title">Warning!</h4>
-        </div>
-        <div class="modal-body">
-          Your current session will expire in 5 minutes. Please Save your changes to continue your session, otherwise you will lose all unsaved data and your session will time out.
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
-      </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
-  </div><!-- /.modal -->
-  
-  
-</div>
+                                            <!-- Button to trigger modal -->
+                                            <a href="#myModal-{{ $currency->id }}" class="btn btn-primary"
+                                                data-toggle="modal">Launch demo modal</a>
 
-                                            <button type="button" class="btn btn-danger"
-                                                onclick="confirmDelete({{ $currency->id }})">Delete</button>
-                                            <a class="btn btn-primary"
-                                                href="{{ route('currency.edit', $currency->id) }}">Edit</a>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                        {{-- {!! $holidays->links() !!} --}}
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="card-footer">
-            <div class="row">
-                <div class="col-7">
-                    <div class="float-left">
-                        <!-- Any additional content you want to add -->
-                    </div>
-                </div>
-                <div class="col-5">
-                    <div class="float-end">
-                        <!-- Any additional content you want to add -->
-                    </div>
-                </div>
-            </div>
-        </div>
+                                            <!-- Modal -->
+                                            <div class="modal fade" id="myModal-{{ $currency->id }}" tabindex="-1"
+                                                role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                                                <div class="modal-dialog">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <button type="button" class="close" data-dismiss="modal"
+                                                                aria-hidden="true">x</button>
+                                                            <h4 class="modal-title">Warning!</h4>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            Your current session will expire in 5 minutes. Please Save your
+                                                            changes to continue your session, otherwise you will lose all
+                                                            unsaved data and your session will time out.
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-default"
+                                                                data-dismiss="modal">Close</button>
+                                                            <button type="button" class="btn btn-primary">Save
+                                                                changes</button>
+                                                        </div>
+                                                    </div><!-- /.modal-content -->
+                                                </div><!-- /.modal-dialog -->
+                                            </div><!-- /.modal -->
 
-        <!-- Confirmation Modal -->
-        <div class="modal fade" id="confirmationModal" tabindex="-1" role="dialog"
-            aria-labelledby="confirmationModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="confirmationModalLabel">Confirm Delete</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        Are you sure you want to delete this currency?
-                    </div>
-                    <div class="modal-footer">
-                        <form id="delete-form" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                            <button type="submit" class="btn btn-danger">Delete</button>
-                        </form>
-                    </div>
+
+                                        </div>
+
+                                        <button type="button" class="btn btn-danger"
+                                            onclick="confirmDelete({{ $currency->id }})">Delete</button>
+                                        <a class="btn btn-primary"
+                                            href="{{ route('currency.edit', $currency->id) }}">Edit</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    {{-- {!! $holidays->links() !!} --}}
                 </div>
             </div>
         </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+    <div class="card-footer">
+        <div class="row">
+            <div class="col-7">
+                <div class="float-left">
+                    <!-- Any additional content you want to add -->
+                </div>
+            </div>
+            <div class="col-5">
+                <div class="float-end">
+                    <!-- Any additional content you want to add -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Confirmation Modal -->
+    <div class="modal fade" id="confirmationModal" tabindex="-1" role="dialog" aria-labelledby="confirmationModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="confirmationModalLabel">Confirm Delete</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    Are you sure you want to delete this currency?
+                </div>
+                <div class="modal-footer">
+                    <form id="delete-form" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
 
 @push('scripts')
     <!-- jQuery and Bootstrap JS -->
-    
+
 
     <script>
-      
+
     </script>
 @endpush
