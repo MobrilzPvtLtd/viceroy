c417ec791ead3e82fc6d36c6a4fedbc52487cfca
Shiv Singh <shiv@srapsware.com>
2024-08-29 17:24:35 +0530

    Update currency index page with improved modal functionality

--- resources/views/backend/currency/index.blade.php
+++ resources/views/backend/currency/index.blade.php
@@ -47,28 +47,32 @@
                                     <td>
 
                                         <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
-</button>
+                                        <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                                            data-bs-target="#delete-confirm-{{ $currency->id }}">
+                                            Launch demo modal
+                                        </button>
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
+                                        <!-- Modal -->
+                                        <div class="modal fade" id="delete-confirm-{{ $currency->id }}" tabindex="-1"
+                                            aria-labelledby="exampleModalLabel-{{ $currency->id }}" aria-hidden="true">
+                                            <div class="modal-dialog">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h5 class="modal-title" id="exampleModalLabel-{{ $currency->id }}">Modal title</h5>
+                                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                            aria-label="Close"></button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        ...
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary"
+                                                            data-bs-dismiss="modal">Close</button>
+                                                        <button type="button" class="btn btn-primary">Save changes</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
 
                                         <button type="button" class="btn btn-danger"
                                             onclick="confirmDelete({{ $currency->id }})">Delete</button>
@@ -100,7 +104,7 @@
     </div>
 
     <!-- Confirmation Modal -->
-    
+
 </div>
 
 @endsection
