498cba93a4a13f6c21c0641f91eb3bbdf6dc984a
satyam8076 <168442979+satyam8076@users.noreply.github.com>
2024-08-29 18:21:36 +0530

    "Removed unnecessary CSS and JavaScript links, refactored delete confirmation modal, and cleaned up HTML structure in currency index view."

--- resources/views/backend/currency/index.blade.php
+++ resources/views/backend/currency/index.blade.php
@@ -1,8 +1,5 @@
 @extends('backend.layouts.app')
 @section('content')
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
@@ -45,9 +42,6 @@
                                     <td>{{ $currency->suffix }}</td>
                                     <td>{{ $currency->bcr }}</td>
                                     <td>
-
-
-
                                         <!-- Modal -->
                                         <div class="modal fade" id="delete-confirm-{{ $currency->id }}" tabindex="-1"
                                             aria-labelledby="exampleModalLabel-{{ $currency->id }}" aria-hidden="true">
@@ -63,19 +57,21 @@
                                                         Do you want to delete: {{ $currency->code }}?
                                                     </div>
                                                     <div class="modal-footer">
-                                                        
-                                                        <button type="button" class="btn btn-danger">Yes</button>
+                                                        <form action="{{ route('currency.destroy', $currency->id) }}" method="POST" id="delete-form-{{ $currency->id }}">
+                                                            @csrf
+                                                            @method('DELETE')
+                                                            <button type="submit" class="btn btn-danger">Yes</button>
+                                                        </form>
                                                         <button type="button" class="btn btn-secondary"
                                                             data-bs-dismiss="modal">No</button>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
-
-                                        <button type="button" class="btn btn-danger" data-bs-toggle="modal"
-                                            data-bs-target="#delete-confirm-{{ $currency->id }}">Delete</button>
                                         <a class="btn btn-primary"
                                             href="{{ route('currency.edit', $currency->id) }}">Edit</a>
+                                        <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                                            data-bs-target="#delete-confirm-{{ $currency->id }}">Delete</button>
                                     </td>
                                 </tr>
                             @endforeach
@@ -86,32 +82,5 @@
             </div>
         </div>
     </div>
-    <div class="card-footer">
-        <div class="row">
-            <div class="col-7">
-                <div class="float-left">
-                    <!-- Any additional content you want to add -->
-                </div>
-            </div>
-            <div class="col-5">
-                <div class="float-end">
-                    <!-- Any additional content you want to add -->
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- Confirmation Modal -->
-
 </div>
-
 @endsection
-
-@push('scripts')
-    <!-- jQuery and Bootstrap JS -->
-
-
-    <script>
-
-    </script>
-@endpush
