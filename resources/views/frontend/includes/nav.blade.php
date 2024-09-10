992789fb67e19c06dc1bf3ef17330020fd8d184f
Ankit Pandita <125086899+AnkitPandita02@users.noreply.github.com>
2024-09-10 10:23:10 +0530

    changes

--- resources/views/frontend/includes/nav.blade.php
+++ resources/views/frontend/includes/nav.blade.php
@@ -28,22 +28,6 @@
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('about') }}">about us</a>
-                        {{-- <ul>
-                            <li>
-                                <a class="menu-item" href="#">User</a>
-                                <ul>
-                                    <li>
-                                        <a class="menu-item" href="">Admin</a>
-                                    </li>
-                                    <li>
-                                        <a class="menu-item"
-                                            href="">Profile</a>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul> --}}
-
-
                     </li>
 
                     <li class="nav-item">
