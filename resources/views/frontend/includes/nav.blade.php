cacd81bc26bb952006624d4613eedad26ca738e7
Shikha Gupta <shikha@mobrilz.agency>
2025-01-03 16:22:36 +0530

    added url for dashboard, profile and logout in navbar

--- resources/views/frontend/includes/nav.blade.php
+++ resources/views/frontend/includes/nav.blade.php
@@ -71,133 +71,123 @@
                             </a>
                         </li>
                     @endguest
-
                     @auth
                         <li>
                             <a class="user_icon" href="{{ route('login') }}">
-
                             </a>
-
-
-                            <!-- <ul>
-                                @can('view_backend')<li><a>Admin</a></li>@endif
-                                <li><a href="{{ route('frontend.users.profile') }}">Profile</a></li>
-                                <li><form id="logout-form" action="{{ route('logout') }}" method="POST">
-                                    {{ csrf_field() }}
-
-                                    <button class="btn btn-text" type="submit">Logout</button>
-                                </form></li>
-                            </ul> -->
-
-                            <div class="dropdown">
+                            <ul>
                                 <button class="dropdown-btn"> <i class="fa fa-user" aria-hidden="true"></i>
                                     {{ Auth::user()->first_name }} </button>
-                                <div class="dropdown-content">
-                                    @can('view_backend') <a>Admin</a>@endif
-                                    <a href="{{ route('frontend.users.profile') }}"> Profile</a>
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST">
-                                        {{ csrf_field() }}
-                                        <button class="btn btn-text" type="submit">Logout</button>
-                                    </form>
+                                 @can('view_backend')
+                                <li>  <a href="{{ route('backend.dashboard') }}" >Admin</a></li>
+                                    @endif
+                                    <li><a href="{{ route('frontend.users.profile') }}">Profile</a></li>
+                                    <li>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                                            {{ csrf_field() }}
+                                         <button class="btn btn-text" type="submit">Logout</button>
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endauth
+
+                        <li>
+                            <div class="d-flex justify-content-center align-items-center">
+                                <div class="content">
+                                    <div class="test-cart" id='cart'>
+                                        <i class="fa fa-heart" aria-hidden="true"></i>
+                                        <p class="notify001" id="cartCount">
+                                            {{ session()->has('cart') ? count(session('cart')) : 0 }}
+                                        </p>
+                                    </div>
+
+                                </div>
+                                <div>
+                                    {{-- //removed unneccessary semi colon  --}}
+                                    <select name="currency" id="currency" class="currency">
+                                        @foreach (App\Models\Currency::get() as $currency)
+                                            <option value="{{ $currency->code }}"
+                                                {{ $currency->code == request()->session()->get('currency') ? 'selected' : '' }}>
+                                                {{ $currency->code }}
+                                            </option>
+                                        @endforeach
+                                    </select>
                                 </div>
                             </div>
 
                         </li>
-                    @endauth
+                    </ul>
+                </div>
 
-                    <li>
-                        <div class="d-flex justify-content-center align-items-center">
-                            <div class="content">
-                                <div class="test-cart" id='cart'>
-                                    <i class="fa fa-heart" aria-hidden="true"></i>
-                                    <p class="notify001" id="cartCount">
-                                        {{ session()->has('cart') ? count(session('cart')) : 0 }}
-                                    </p>
-                                </div>
-
-                            </div>
-                            <div>
-                                {{-- //removed unneccessary semi colon  --}}
-                                <select name="currency" id="currency" class="currency">
-                                    @foreach (App\Models\Currency::get() as $currency)
-                                        <option value="{{ $currency->code }}"
-                                            {{ $currency->code == request()->session()->get('currency') ? 'selected' : '' }}
-                                            >{{ $currency->code }}
-                                        </option>
-                                    @endforeach
-                                </select>
-                            </div>
-                        </div>
-
-                    </li>
-                </ul>
             </div>
 
-        </div>
 
+            <style>
+                /* Dropdown Button */
+                .dropdown-btn {
+                    background-color: #fff;
+                    border: none;
+                    cursor: pointer;
+                }
 
-        <style>
-            /* Dropdown Button */
-            .dropdown-btn {
-                background-color: #fff;
-                border: none;
-                cursor: pointer;
-            }
+                /* Dropdown Content (Hidden by Default) */
+                .dropdown-content {
+                    display: none;
+                    position: absolute;
+                    background-color: #f9f9f9;
+                    min-width: 160px;
+                    width: fit-content;
+                    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+                    z-index: 1;
+                }
 
-            /* Dropdown Content (Hidden by Default) */
-            .dropdown-content {
-                display: none;
-                position: absolute;
-                background-color: #f9f9f9;
-                min-width: 160px;
-                width: fit-content;
-                box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-                z-index: 1;
-            }
+                /* Links Inside the Dropdown */
+                .dropdown-content a {
+                    color: black;
+                    padding: 12px 16px;
+                    text-decoration: none;
+                    display: block;
+                }
 
-            /* Links Inside the Dropdown */
-            .dropdown-content a {
-                color: black;
-                padding: 12px 16px;
-                text-decoration: none;
-                display: block;
-            }
+                /* Change Color of Links on Hover */
+                .dropdown-content a:hover {
+                    background-color: #ddd;
+                }
 
-            /* Change Color of Links on Hover */
-            .dropdown-content a:hover {
-                background-color: #ddd;
-            }
+                /* Show the Dropdown Content When Hovered Over Dropdown Button */
+                .dropdown:hover .dropdown-content {
+                    display: block;
+                }
 
-            /* Show the Dropdown Content When Hovered Over Dropdown Button */
-            .dropdown:hover .dropdown-content {
-                display: block;
-            }
+                button.dropdown-btn {
+                    color: #ffffff;
+                    background-color: black;
+                }
 
-            button.dropdown-btn {
-                color: #ffffff;
-                background-color: black;
-            }
+                .dropdown {
+                    margin-top: -20px;
+                }
 
-            .dropdown {
-                margin-top: -20px;
-            }
-            .content {
+                .content {
 
-                position: relative !important;
-                width: 100px !important;
-                z-index: 1 !important;
-            }
-            p.notify001 {
-                top: -15px !important;
-            }
-            .currency {
-                background-color: #e6b025;
-                padding: 5px;
-                border-radius: 5px;
-                font-weight: 700;
-                margin-left: -25px
-            }
-        </style>
+                    position: relative !important;
+                    width: 100px !important;
+                    z-index: 1 !important;
+                }
 
-        @include('frontend.includes.script')
-    </nav>
+                p.notify001 {
+                    top: -15px !important;
+                }
+
+                .currency {
+                    background-color: #e6b025;
+                    padding: 5px;
+                    border-radius: 5px;
+                    font-weight: 700;
+                    margin-left: -25px
+                }
+            </style>
+
+            @include('frontend.includes.script')
+        </nav>
